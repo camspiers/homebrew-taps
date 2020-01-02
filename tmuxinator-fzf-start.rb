6202b5b23990268249fef15345cab14187439c87
Cam Spiers <cameron.spiers@heyday.co.nz>
2020-01-03 11:56:04 +1300

    Fix classname

--- tmuxinator-fzf-start.rb
+++ tmuxinator-fzf-start.rb
@@ -1,4 +1,4 @@
-class Tmuxinatorfzfstart < Formula
+class TmuxinatorFzfStart < Formula
   desc "A macOS tool for starting then waiting for Docker.app to be ready"
   homepage "https://github.com/camspiers/tmuxinator-fzf-start"
   url "https://github.com/camspiers/tmuxinator-fzf-start/archive/1.0.0.tar.gz"

c3563ab84eb0a6799d42826012812edbe3ae77af
Cam Spiers <cameron.spiers@heyday.co.nz>
2020-01-03 18:12:17 +1300

    New tmuxinator-fzf-start release

--- tmuxinator-fzf-start.rb
+++ tmuxinator-fzf-start.rb
@@ -1,8 +1,8 @@
 class TmuxinatorFzfStart < Formula
   desc "A macOS tool for starting then waiting for Docker.app to be ready"
   homepage "https://github.com/camspiers/tmuxinator-fzf-start"
-  url "https://github.com/camspiers/tmuxinator-fzf-start/archive/1.0.0.tar.gz"
-  sha256 "d2f26efc52ceecb85afdf4ed74674d93a7f9d240eb6942e46b16848acef90459"
+  url "https://github.com/camspiers/tmuxinator-fzf-start/archive/1.0.1.tar.gz"
+  sha256 "c1a85e8dffbf6cde3b5e0346667dfd952cda5fdba5736ba41ffc8fa7f2aad91b"
 
   bottle :unneeded
 
